f073c57bb42e8dc62c680e31ded83500d544399f
ben slama <ben.slama.mohamed.ali@gmail.com>
2016-11-02 15:54:59 +0100

    Modification test DB

--- var/www/index.php
+++ var/www/index.php
@@ -1,5 +1,34 @@
 <?php
+$hostname = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "test";
+ 
+//try {
+        /*$pdo = new PDO("mysql:host=$hostname", $username, $password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+	$dbname = "`".str_replace("`","``",$dbname)."`";
+	$pdo->query("CREATE DATABASE IF NOT EXISTS $dbname");
+	$pdo->query("use $dbname");*/
+echo "eee";
+
+// connexion à Mysql sans base de données
+$pdo = new PDO('mysql:host='.$hostname, $username, $password);
+ 
+// création de la requête sql
+// on teste avant si elle existe ou non (par sécurité)
+$requete = "CREATE DATABASE IF NOT EXISTS `".$dbname."` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+ 
+// on prépare et on exécute la requête
+$pdo->prepare($requete)->execute();
+
+
+  //  }
+//catch(PDOException $e)
+   // {
+   // echo $e->getMessage();
+    //}
 phpinfo();
 
 
