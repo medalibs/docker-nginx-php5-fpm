2039509b9f742622afd0427507a8bc0fce11621c
ben slama <ben.slama.mohamed.ali@gmail.com>
2016-11-03 12:09:38 +0100

    Modification params database

--- var/www/index.php
+++ var/www/index.php
@@ -1,21 +1,21 @@
 <?php
-$hostname = "localhost";
+$hostname = "192.168.1.52:3305";
 $username = "root";
 $password = "root";
 $dbname = "test";
  
-//try {
+try {
         /*$pdo = new PDO("mysql:host=$hostname", $username, $password);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	$dbname = "`".str_replace("`","``",$dbname)."`";
 	$pdo->query("CREATE DATABASE IF NOT EXISTS $dbname");
 	$pdo->query("use $dbname");*/
-echo "eee";
+
 
 // connexion à Mysql sans base de données
 $pdo = new PDO('mysql:host='.$hostname, $username, $password);
- 
+
 // création de la requête sql
 // on teste avant si elle existe ou non (par sécurité)
 $requete = "CREATE DATABASE IF NOT EXISTS `".$dbname."` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
@@ -24,11 +24,11 @@ $requete = "CREATE DATABASE IF NOT EXISTS `".$dbname."` DEFAULT CHARACTER SET ut
 $pdo->prepare($requete)->execute();
 
 
-  //  }
-//catch(PDOException $e)
-   // {
-   // echo $e->getMessage();
-    //}
+    }
+catch(PDOException $e)
+    {
+    echo $e->getMessage();
+    }
 phpinfo();
 
 
